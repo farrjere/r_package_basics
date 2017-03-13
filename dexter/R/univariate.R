9056cd09a38995d1e8b0056b58dbaccb42e0e447
Jeremy Farrell <farrjere@isu.edu>
2017-03-12 23:20:30 -0600

    1.3 Univariate Summaries

--- dexter/R/univariate.R
+++ dexter/R/univariate.R
@@ -12,3 +12,21 @@ univariate_analysis <-function(input_data, continuous_cols, continuous_threshold
   not_continuous = !(names(input_data) %in% continuous_cols)
   cat_cols = names(input_data[,not_continuous,with=FALSE])
 }
+
+univariate_summaries <- function(input_data, continuous_cols, categorical_cols){
+  continuous_summaries = lapply(continuous_cols, function(x){
+    x = as.name(x)
+    input_data[,list(min=min(eval(x)),
+                     lower=quantile(eval(x), 0.25), 
+                     median = median(eval(x)), 
+                     upper=quantile(eval(x), 0.75), 
+                     max=max(eval(x)), 
+                     mean=mean(eval(x)), 
+                     std_dev=sd(eval(x)), 
+                     skew=moments::skewness(eval(x)), 
+                     kurtosis=moments::kurtosis(eval(x))),]
+  })
+  
+  categorical_summaries = lapply(categorical_cols, function(x){input_data[,.N, x]})
+  return(list(categorical_summaries=categorical_summaries, continuous_summaries=continuous_summaries))
+}
