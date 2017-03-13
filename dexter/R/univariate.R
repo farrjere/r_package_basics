8f0998c3b89e070b01cd3ab4a18e78863cdb298e
Jeremy Farrell <farrjere@isu.edu>
2017-03-12 23:22:45 -0600

    1.4 Univariate Plots

--- dexter/R/univariate.R
+++ dexter/R/univariate.R
@@ -30,3 +30,33 @@ univariate_summaries <- function(input_data, continuous_cols, categorical_cols){
   categorical_summaries = lapply(categorical_cols, function(x){input_data[,.N, x]})
   return(list(categorical_summaries=categorical_summaries, continuous_summaries=continuous_summaries))
 }
+
+univariate_plots <- function(input_data, continuous_cols, categorical_cols){
+  #Produce are categorical plots
+  cat_bars = lapply(categorical_cols, function(x){
+    ggplot2::ggplot(input_data) +
+      #aes_q and as.name are to get ggplot2 to work well with a string variable
+      ggplot2::geom_bar(ggplot2::aes_q(as.name(x)), fill='skyblue')+
+      ggplot2::theme(panel.background = ggplot2::element_rect(fill='white'))
+  })
+  
+  continuous_boxes = lapply(continuous_cols, function(x){
+    ggplot2::ggplot(input_data) +
+      #geom_boxplot needs an x and y in its aesthetic, factor(0) just gives it a dummy factor
+      ggplot2::geom_boxplot(ggplot2::aes_q(x=factor(0), as.name(x)), fill='skyblue', varwidth = TRUE)+
+      #I like boxplots to be going side to side not up and down, you decide
+      ggplot2::coord_flip()+
+      ggplot2::theme(panel.background = ggplot2::element_rect(fill='white'),
+                     axis.title.y = ggplot2::element_blank())
+  })
+  
+  continuous_hists = lapply(continuous_cols, function(x){
+    ggplot2::ggplot(input_data) +
+      ggplot2::geom_histogram(ggplot2::aes_q(as.name(x)), fill='skyblue')+
+      ggplot2::theme(panel.background = ggplot2::element_rect(fill='white'))
+  })
+  
+  return(list(categorical_bar_charts = cat_bars, 
+              continuous_box_plots = continuous_boxes, 
+              continuous_histograms = continuous_hists))
+}
